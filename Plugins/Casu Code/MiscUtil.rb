dc57422480e65e0b0d3e8cd76f87e8874e10841f
Casu <alexander.krischer@icloud.com>
2022-01-28 18:52:07 +0100

    refactor

--- Plugins/Casu Code/MiscUtil.rb
+++ Plugins/Casu Code/MiscUtil.rb
@@ -1,30 +1,28 @@
-
 def pbDeleteFainted
-    n = $Trainer.party.length
-    for i in (0..n)
-      $Trainer.party.delete_at(n-i) if $Trainer.party[n-i] && !$Trainer.party[n-i].able?
-    end
+  n = $Trainer.party.length
+  (0..n).each do |i|
+    $Trainer.party.delete_at(n - i) if $Trainer.party[n - i] && !$Trainer.party[n - i].able?
   end
+end
 
 # Not used
 def pbGiveAllExp(amount = 500)
-    lvls = []
-  
-    $Trainer.party.each_with_index do |pkmn,i| 
-      lvls[i] = pkmn.level
-      pbGiveOneExp(pkmn, amount)
-    end
-  
-    pbEvolutionCheck(lvls)
-end 
+  lvls = []
+
+  $Trainer.party.each_with_index do |pkmn, i|
+    lvls[i] = pkmn.level
+    pbGiveOneExp(pkmn, amount)
+  end
+
+  pbEvolutionCheck(lvls)
+end
 
 # Not used
 def pbGiveOneExp(pkmn, amount)
-    level_bevor = pkmn.level
-    pkmn.exp += amount
-    if pkmn.level > level_bevor
-        print("level up")
-        pkmn.calc_stats
-    end
+  level_bevor = pkmn.level
+  pkmn.exp += amount
+  if pkmn.level > level_bevor
+    print('level up')
+    pkmn.calc_stats
+  end
 end
-  
--- Plugins/Casu Code/PokemonGeneration.rb
+++ Plugins/Casu Code/PokemonGeneration.rb
@@ -1,62 +1,66 @@
 POKEMON_GET_LEVEL = [15, 30, 60, 90]
 
 def pbGenPokeChoice
-    stages_cleared = pbGet(48)
-    lvl = POKEMON_GET_LEVEL[stages_cleared]
-  
-    pkmn = pbChooseRandomPokemon(
-    whiteList=nil,
-    blackList="suggested",
-    addList=nil,
-    base_only=true,
-    choose_gen=[1,2,3,4,5])
-    
-    pbSet(26, pbGetCorrectEvo(pkmn, lvl))
-    
-    pkmn = pbChooseRandomPokemon(
-    whiteList=nil,
-    blackList="suggested",
-    addList=nil,
-    base_only=true,
-    choose_gen=[1,2,3,4,5])
-    
-    pbSet(27, pbGetCorrectEvo(pkmn, lvl))
-    
-    pkmn = pbChooseRandomPokemon(
-    whiteList=nil,
-    blackList="suggested",
-    addList=nil,
-    base_only=true,
-    choose_gen=[1,2,3,4,5])
-    
-    pbSet(28, pbGetCorrectEvo(pkmn, lvl))
-  end
+  stages_cleared = pbGet(48)
+  lvl = POKEMON_GET_LEVEL[stages_cleared]
 
-  def pbGetCorrectEvo(pkmn, lvl)
-    res = pkmn
-    nxt = pkmn
-    while nxt
-      res = nxt
-      nxt = pbGenPkmn(nxt, lvl).check_evolution_on_level_up
-    end
-    return res
-  end
+  pkmn = pbChooseRandomPokemon(
+    whiteList = nil,
+    blackList = 'suggested',
+    addList = nil,
+    base_only = true,
+    choose_gen = [1, 2, 3, 4, 5]
+  )
 
-  def pbGenStarterPkmn(type)
-    pkmn = pbChooseRandomPokemon(
-      whiteList=[:BULBASAUR, :CHARMANDER, :SQUIRTLE, :CHIKORITA, :CYNDAQUIL, :TOTODILE, :TREECKO, :TORCHIC, :MUDKIP, :TURTWIG, :CHIMCHAR, :PIPLUP, :SNIVY, :TEPIG, :OSHAWOTT], 
-      blackList="suggested", 
-      addList=nil,                          
-      base_only=true, 
-      choose_gen=[1,2,3,4,5], 
-      typeWhitelist=[type]
-      )
+  pbSet(26, pbGetCorrectEvo(pkmn, lvl))
+
+  pkmn = pbChooseRandomPokemon(
+    whiteList = nil,
+    blackList = 'suggested',
+    addList = nil,
+    base_only = true,
+    choose_gen = [1, 2, 3, 4, 5]
+  )
+
+  pbSet(27, pbGetCorrectEvo(pkmn, lvl))
+
+  pkmn = pbChooseRandomPokemon(
+    whiteList = nil,
+    blackList = 'suggested',
+    addList = nil,
+    base_only = true,
+    choose_gen = [1, 2, 3, 4, 5]
+  )
+
+  pbSet(28, pbGetCorrectEvo(pkmn, lvl))
+end
+
+def pbGetCorrectEvo(pkmn, lvl)
+  res = pkmn
+  nxt = pkmn
+  while nxt
+    res = nxt
+    nxt = pbGenPkmn(nxt, lvl).check_evolution_on_level_up
   end
-  
-  def pbGiveRandomPoke(saveSlot)
-    stages_cleared = pbGet(48)
-    lvl = lvl = POKEMON_GET_LEVEL[stages_cleared]
-    
-    pkmn = pbGenPkmn(pbGet(saveSlot), lvl)
-    pbAddPokemon(pkmn)
-  end
+  res
+end
+
+def pbGenStarterPkmn(type)
+  pkmn = pbChooseRandomPokemon(
+    whiteList = %i[BULBASAUR CHARMANDER SQUIRTLE CHIKORITA CYNDAQUIL TOTODILE TREECKO TORCHIC MUDKIP
+                   TURTWIG CHIMCHAR PIPLUP SNIVY TEPIG OSHAWOTT],
+    blackList = 'suggested',
+    addList = nil,
+    base_only = true,
+    choose_gen = [1, 2, 3, 4, 5],
+    typeWhitelist = [type]
+  )
+end
+
+def pbGiveRandomPoke(saveSlot)
+  stages_cleared = pbGet(48)
+  lvl = lvl = POKEMON_GET_LEVEL[stages_cleared]
+
+  pkmn = pbGenPkmn(pbGet(saveSlot), lvl)
+  pbAddPokemon(pkmn)
+end
