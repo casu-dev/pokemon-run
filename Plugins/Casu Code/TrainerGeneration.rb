fc76794355089dc453fd71dc6894db4bccfeb726
Casu <alexander.krischer@icloud.com>
2022-02-07 11:17:56 +0100

    dynamic trainer gen

---
+++ Plugins/Casu Code/TrainerGeneration.rb
@@ -0,0 +1,44 @@
+TRAINER_OVERRIDE = [
+  {
+    tType: :YOUNGSTER, tName: 'Trainer',
+    lvl1: 10, lvl2: 12, numPkmn: 2,
+    pkmnPool: %i[BIDOOF BUNNELBY EEVEE LILLIPUP AIPOM BUNEARY MEOWTH MINCCINO RATTATA PATRAT]
+  },
+  {
+    tType: :LASS, tName: 'Trainer',
+    lvl1: 25, lvl2: 28, numPkmn: 3,
+    pkmnPool: %i[GLOOM SEADRA LAMPENT STARAVIA KIRLIA GRAVELER MARSHTOMP LOUDRED POLIWHIRL NUZLEAF]
+  },
+  {
+    tType: :COOLTRAINER_M, tName: 'Trainer',
+    lvl1: 55, lvl2: 59, numPkmn: 3,
+    pkmnPool: %i[ARCANINE AZUMARILL ABOMASNOW SLOWBRO HITMONTOP STARMIE BRONZONG MACHAMP HONCHKROW GALVANTULA TOGEKISS RHYDON SCRAFTY ROSERADE AZUMARILL ]
+  },
+  {
+    tType: :COOLTRAINER_F, tName: 'Trainer',
+    lvl1: 80, lvl2: 85, numPkmn: 3,
+    pkmnPool: %i[ALAKAZAM CONKELDURR EXCADRILL FERROTHORN GLISCOR GENGAR JELLICENT MAMOSWINE REUNICLUS SCIZOR STARMIE TENTACRUEL HIPPOWDON HAXORUS INFERNAPE LUCARIO METAGROSS ABOMASNOW]
+  },
+]
+
+Events.onTrainerPartyLoad += proc { |_sender, trainer_list|
+  return unless trainer_list
+
+  trainer_list.each do |trainer|
+    template = TRAINER_OVERRIDE.find { |i| i[:tType] == trainer.trainer_type and i[:tName] == trainer.name }
+    next unless template # Do nothing if no override was found
+
+    # Determin lvl
+    lvl = template[:lvl1]
+    lvl = template[:lvl2] if pbGet(49) + 2 > Settings::ROOMS_PER_STAGE / 2
+
+    # Generate Party
+    newParty = []
+    while newParty.length < template[:numPkmn]
+      pkmnPool = template[:pkmnPool].reject { |p| newParty.include? p }
+      newParty.push(Pokemon.new(pkmnPool[rand(pkmnPool.length)], lvl))
+    end
+
+    trainer.party = newParty
+  end
+}
